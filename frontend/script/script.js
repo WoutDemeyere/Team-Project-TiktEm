60d403b503d52e67cf5e71e836291257a16b467d
WoutDemeyere <wout.demeyere@student.howest.be>
2021-01-13 18:44:56 +0100

    Finished menu selection

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -2,57 +2,27 @@ var mainContent, singleplayerButton, multiplayerButton, singleplayerContainer, m
 
 const listenToCollapsables = () => {
     singleplayerButton.addEventListener('click', function() {
-        foldMulti()
-        collapseSingle();
-        
-    })
+        this.parentNode.style.flexGrow = "1";
+        multiplayerButton.parentNode.style.flexGrow = "0";
+        mainContent.classList.add('c-collapsed')
+    })  
 
     multiplayerButton.addEventListener('click', function() {
-        foldSingle();
-        collapseMulti();
-       
-    })
-}
-
-const collapseSingle  = function() {
-    mainContent.classList.add('c-collapsed');
-    singleplayerContainer.classList.add("c-icon-button-container--collapsed");
-    singleplayerButton.style.display = "none";
-    document.querySelector('.js-single-hidden-options').style.height = "100%";
-}
-
-const foldSingle = function() {
-    singleplayerContainer.classList.remove("c-icon-button-container--collapsed");
-    singleplayerButton.style.display = "block";
-    document.querySelector('.js-single-hidden-options').style.height = "0";
-}
- 
-const collapseMulti = function() {
-    mainContent.classList.add('c-collapsed');
-    multiplayerContainer.classList.add("c-icon-button-container--collapsed");
-    multiplayerButton.style.display = "none";
-    document.querySelector('.js-multi-hidden-options').style.height = "100%";
-}
-
-const foldMulti = function() {
-    multiplayerContainer.classList.remove("c-icon-button-container--collapsed");
-    multiplayerButton.style.display = "block";
-    document.querySelector('.js-multi-hidden-options').style.height = "0";
+        this.parentNode.style.flexGrow = "1";
+        singleplayerButton.parentNode.style.flexGrow = "0";
+        mainContent.classList.add('c-collapsed')
+    })  
 }
 
 const getDomElements = function() {
     mainContent = document.querySelector('.js-main-content');
     singleplayerButton = document.querySelector('.js-single');
     multiplayerButton = document.querySelector('.js-multi');
-
-    singleplayerContainer = document.querySelector('.js-single-container');
-    multiplayerContainer = document.querySelector('.js-multi-container');
 }
 
 const init = () => {
     console.log('Script loaded!')
     getDomElements();
-
     listenToCollapsables();
 }
 
